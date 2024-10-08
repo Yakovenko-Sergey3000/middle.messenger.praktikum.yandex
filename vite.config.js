1524a16405a94b50e21c4287ecc70d77f4b0602f
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-10-08 17:37:44 +0300

    исправил vite.config файл и перенес index.html  в корень

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import {resolve} from "path"
 import handlebars from "vite-plugin-handlebars";
-
 export default defineConfig({
-  root: resolve(__dirname, "src"),
   build: {
     rollupOptions: {
       input: resolve(__dirname, "index.html")
