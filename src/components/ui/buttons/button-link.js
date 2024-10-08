85dbc28e79a1ea77460ec070f58f3e75de0ac666
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-10-09 00:51:23 +0300

    Установить тип по умолчанию для кнопки

--- src/components/ui/buttons/button-link.js
+++ src/components/ui/buttons/button-link.js
@@ -3,7 +3,7 @@ import Handlebars from "handlebars";
 import buttonTemplate from "@ui/buttons/button.tmpl.js";
 
 const tmp = Handlebars.compile(buttonTemplate);
-export default ({ label, type, className = "" }) =>
+export default ({ label, type = "button", className = "" }) =>
   tmp({
     type,
     className: `button-link ${className}`,
