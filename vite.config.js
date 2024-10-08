7eca93549f07b8cf4d29a7e2e6d4040e6f1b1e2d
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-10-08 17:45:27 +0300

    добавил alias в config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import {resolve} from "path"
+import { defineConfig } from "vite";
+import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: resolve(__dirname, "index.html")
-    }
+      input: resolve(__dirname, "index.html"),
+    },
   },
-  plugins: [handlebars()]
-})
-
+  plugins: [handlebars()],
+  resolve: {
+    alias: {
+      "@widgets": resolve(__dirname, "./src/components/widgets"),
+    },
+  },
+});
