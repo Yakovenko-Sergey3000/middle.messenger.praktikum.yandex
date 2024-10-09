86ae0cc7fac2560c476f2ad28dc86db751c54050
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-10-09 08:22:19 +0300

    Убрал лишний compile на страницах sign on и sign in

--- src/pages/sign-out-page/main.js
+++ src/pages/sign-out-page/main.js
@@ -1,7 +1,4 @@
 import "@global-style";
-import Handlebars from "handlebars";
 import { moduleSignOut } from "@modules/auth/sign-out/index.js";
 
-const tpm = Handlebars.compile(moduleSignOut());
-
-document.querySelector("#app").innerHTML = tpm();
+document.querySelector("#app").innerHTML = moduleSignOut();
